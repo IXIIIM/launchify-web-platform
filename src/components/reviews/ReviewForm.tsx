915cd412e62482924ad84b1377e2237a653b519b
IXIIIM <ixiiim@gmail.com>
2025-02-17 11:17:04 -0600

    Add ReviewForm component with StarRating subcomponent

--- src/components/reviews/ReviewForm.tsx
+++ src/components/reviews/ReviewForm.tsx
@@ -105,4 +105,27 @@ const ReviewForm = ({ onSubmit, onCancel }) => {
   );
 };
 
+const StarRating = ({ value, onChange, size = 'default' }) => {
+  return (
+    <div className="flex space-x-1">
+      {[1, 2, 3, 4, 5].map((star) => (
+        <button
+          key={star}
+          type="button"
+          onClick={() => onChange(star)}
+          className={`focus:outline-none ${
+            size === 'small' ? 'p-1' : 'p-2'
+          }`}
+        >
+          <Star
+            className={`${
+              star <= value ? 'fill-yellow-400 text-yellow-400' : 'text-gray-300'
+            } ${size === 'small' ? 'w-4 h-4' : 'w-6 h-6'}`}
+          />
+        </button>
+      ))}
+    </div>
+  );
+};
+
 export default ReviewForm;
